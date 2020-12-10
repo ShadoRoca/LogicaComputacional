ba347a9e79ebe77967ad721ef0495f4fae700d89
Rodrigo <godites@live.com>
2020-12-09 19:02:43 -0600

    Ejercicio 3

--- practica05/Paradojas/Practica05/Practica05.pl
+++ practica05/Paradojas/Practica05/Practica05.pl
@@ -5,7 +5,20 @@ elemento(E,[_|L]) :- elemento(E,L).
 
 %2.
 
-%3.
+
+
+%3. Relación palindromo.
+
+%Añade un elemento a una lista.
+concatena(E,[],[E]).
+concatena(E,[HEAD|T], [HEAD|R]) :- concatena(E,T,R). 
+
+%Nos da la reversa de una lista
+reversa([],[]).
+reversa([E],[E]).
+reversa([HEAD|T], R) :- reversa(T,L), concatena(HEAD,L,R).
+
+palindromo(L,R) :- reversa(L,R).
 
 %4.
 
