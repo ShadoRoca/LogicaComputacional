31fb6c1a1652e3fc694d7d2807cc4c690545e68c
itzel89 <itzel_tinoco@ciencias.unam.mx>
2021-01-19 13:40:14 -0600

    Agregando los ejercicios impares

--- practica06/Paradojas/Practica06/Practica06.pl
+++ practica06/Paradojas/Practica06/Practica06.pl
@@ -1,5 +1,8 @@
 /*
-
+*Nombre del equipo: Paradojas*
+--Liprandi Cortes Rodrigo 317275605
+--Tinoco Miguel Laura Itzel 316020189
+-- *Práctica 06*
 */
 
 % 1.- Relación Traducir. 
@@ -37,7 +40,22 @@ traducir([H|T], L) :- dicc(H,X), append([X],L1,L), traducir(T,L1).
 
 
 % 2.- Mundo de los cubos.
+%definimos el mundo de los cubos
+cubo(azul).
+cubo(rojo).
+cubo(gris).
+cubo(verde).
+cubo(amarillo).
+cubo(morado).
+sobre(gris,rojo).
+sobre(rojo,azul).
+sobre(amarillo,morado).
 
+%definimos las reglas
+hastaArriba(X) :- not(bloqueado(X)).
+bloqueado(X) :- sobre(_,X).
+hastaAbajo(X) :- not(sobre(X,_)).
+mover(X,Y) :- not(bloqueado(X)), hastaArriba(Y).
 
 % 3.- AFND.
 
@@ -59,4 +77,20 @@ transicion(E,[H|T]) :- delta(E,H,EA), transicion(EA,T).
 
 aceptar(S) :- transicion(q0,S).
 
-% 4.- Relación Mezclar.
+% 4.- Relación Mezclar, mezcla dos listas previamente ordendas.
+mezclar(L1,L2,L) :- ordenar(L1,X), ordenar(L2,Y), mezclarAux(X,Y,L).
+
+%relacion auxiliar para mezclar
+mezclarAux([],B,B).
+mezclarAux(A,[],A).
+mezclarAux([A|As], [B|Bs], [A|Rs]) :- A =< B, mezclarAux(As, [B|Bs], Rs),!.
+mezclarAux([A|As], [B|Bs], [B|Rs]) :- mezclarAux([A|As], Bs, Rs).
+
+%relación ordenar que ordena por insercion
+ordenar([],[]).
+ordenar([X|Xs],Ys) :- ordenar(Xs,Xs0), inserta(X,Xs0,Ys).
+
+%relación auxiliar inserta un elemento en una lista ordenada.
+inserta(X,[],[X]).
+inserta(X,[Y|Ys],[X,Y|Ys]) :- X < Y.
+inserta(X,[Y|Ys],[Y|Zs]) :- X >= Y, inserta(X,Ys,Zs).
